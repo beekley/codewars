ca48a01c1438e57678c386e24698e7035e96b8c2
Brett Beekley <brettbeekley@gmail.com>
2017-09-06 15:42:34 -0700

    Make request to Twitter

--- angular-challenge/src/app.js
+++ angular-challenge/src/app.js
@@ -1,14 +1,31 @@
 const testApp = angular.module('testApp', []);
 
 testApp.controller('testController', $scope => {
-  $scope.testData= [
-    {
-      name: "Brett",
-      type: "Homebrewer"
+  const success = data => {
+    console.log(data);
+    $scope.testData= [
+      {
+        name: secrets.consumerSecret,
+        type: secrets.consumerKey
+      },
+      {
+        name: "Pizza",
+        type: "Hawaiian"
+      }
+    ];
+  };
+  
+  const error = (data, textStatus) => console.log(data);
+  
+  const getTweet = $.ajax({
+    headers: {
+      Authorization: 'Bearer ' + secrets.accessToken
     },
-    {
-      name: "Pizza",
-      type: "Hawaiian"
-    }
-  ];
+    url: 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=jackdriper&count=1&callback=?',
+    type: 'GET',
+    dataType: 'jsonp',
+    data: {},
+    error,
+    success
+  });
 });
